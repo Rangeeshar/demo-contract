1cc3559395ef8998ed12e91280e37550a7024b3b
slowpon <slowpon@outlook.com>
2022-11-23 16:54:07 -0500

    update interface

--- contracts/interfaces/IWETHGateway.sol
+++ contracts/interfaces/IWETHGateway.sol
@@ -3,10 +3,25 @@ pragma solidity 0.8.17;
 
 interface IWETHGateway {
 
+  /**
+   * @param {onBehalfOf} depositor
+   * @param {depositPeriod} 0 -> variable period; 1 -> fixed period (3 months)
+   */
   function depositETH(address onBehalfOf, uint8 depositPeriod) external payable;
 
+  /**
+   * @param {amount} withdrawal amount
+   * @param {to} address where fund goes to
+   * @param {depositPeriod} 0 -> variable period; 1 -> fixed period (3 months)
+   */
   function withdrawETH(uint256 amount, address to, uint8 depositPeriod) external;
 
+  /**
+   * @param {amount} withdrawal amount
+   * @param {nftAsset} NFT collection address
+   * @param {nftTokenId} NFT token ID
+   * @param {onBehalfOf} depositor
+   */
   function borrowETH(
     uint256 amount,
     address nftAsset,
@@ -14,9 +29,12 @@ interface IWETHGateway {
     address onBehalfOf
   ) external;
 
+  /**
+   * @param {nftAsset} NFT collection address
+   * @param {nftTokenId} NFT token ID
+   */
   function repayETH(
     address nftAsset,
     uint256 nftTokenId
   ) external payable returns ( bool);
-
 }
